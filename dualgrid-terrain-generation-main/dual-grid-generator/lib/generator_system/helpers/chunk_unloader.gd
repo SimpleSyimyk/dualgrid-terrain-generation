f3561755c55084204fc8ad21480b6bd0293c7b0d
SimpleSyimyk <117256036+SimpleSyimyk@users.noreply.github.com>
2025-09-22 18:08:02 +0600

    Add files via upload

---
+++ dualgrid-terrain-generation-main/dual-grid-generator/lib/generator_system/helpers/chunk_unloader.gd
@@ -0,0 +1,50 @@
+class_name ChunkUnloader extends Node
+
+var batch_size: int = 50
+var to_load_chunk: Array[Vector2i] = []
+var loaded_chunks: Dictionary[Vector2i, String] = {}
+var current_index: int = 0
+var biome_nodes: Dictionary[String, DualGrid]
+var noise_buffer: PackedByteArray
+var biome_names: Array[String] = []
+var params_names: Array[String] = []
+var terrain_noise: FastNoiseLite = null
+var biomes_params_cache: Dictionary[int, PackedByteArray] = {}
+var biomes_keys: Array[Vector2i] = []
+
+
+func _init(
+	_to_load_chunk: Array[Vector2i],
+	_loaded_chunks: Dictionary[Vector2i, String],
+	_biome_nodes: Dictionary[String, DualGrid],
+	_noise_buffer: PackedByteArray,
+	_biome_names: Array[String],
+	_params_names: Array[String],
+	_terrain_noise: FastNoiseLite,
+	_biomes_params_cache: Dictionary[int, PackedByteArray]
+	
+	):
+	to_load_chunk = _to_load_chunk
+	loaded_chunks = _loaded_chunks
+	biome_nodes = _biome_nodes
+	noise_buffer = _noise_buffer
+	biome_names = _biome_names
+	params_names = _params_names
+	terrain_noise = _terrain_noise
+	biomes_params_cache = _biomes_params_cache
+	biomes_keys = loaded_chunks.keys()
+	
+func process_batch():
+	var end_index: int = min(current_index + batch_size, biomes_keys.size())
+
+	for i in range(current_index, end_index):
+		var target_chunk_coords: Vector2i = biomes_keys[i]
+		if target_chunk_coords in to_load_chunk: continue
+		var biome_name: String = loaded_chunks[target_chunk_coords]
+		loaded_chunks.erase(target_chunk_coords)
+		biome_nodes[biome_name].auto_erase(target_chunk_coords)
+	
+	current_index = end_index 
+	if current_index >= to_load_chunk.size():
+		return true
+	return false
---
+++ dualgrid-terrain-generation-main/dual-grid-generator/lib/generator_system/resurses/procedural_generator_resource.gd
@@ -0,0 +1,98 @@
+@tool
+extends Resource
+class_name GeneratorResource
+
+@export_group("Distance and Size")
+var tiles: Array[DualGridResource]
+@export_group('Setup generator')
+
+@export var params_names: Array[String] = []
+@export var biomes_names: Array[String] = []
+@export_tool_button("Setup biomes")
+var btn: Callable = inspector_button
+@export_group("Bioms")
+var biomes: Dictionary[String, DualGridResource] = {}
+
+func inspector_button() -> void:
+	notify_property_list_changed()
+	_generate_biomes_resource()
+
+func get_biomes_params_cache() -> Dictionary[int, PackedByteArray]:
+	var biomes_params_cache: Dictionary[int, PackedByteArray] = {}
+	for biom_index: int in biomes_names.size():
+		var biome_name: String = biomes_names[biom_index]
+		var biom_params = _get_biome_params(biome_name)
+		
+		var arr := PackedByteArray()
+		arr.resize(params_names.size())
+		for i in params_names.size():
+			arr[i] = biom_params[params_names[i]]
+		
+		biomes_params_cache[biom_index] = arr
+	return biomes_params_cache
+
+func get_biomes_params() -> Dictionary[String, Dictionary]:
+	var biomes_params: Dictionary[String, Dictionary] = {}
+	
+	for biome_name in biomes.keys():
+		biomes_params[biome_name] = _get_biome_params(biome_name)
+	
+	return biomes_params
+
+func get_boimes_pathes() -> Dictionary[String, NodePath]:
+	var pathes: Dictionary[String, NodePath] = {}
+	for biome_name in biomes.keys(): 
+		pathes[biome_name] = biomes[biome_name].path
+	return pathes
+
+func _get_biom_path(biom_name: String) -> String:
+	if biomes.has(biom_name):
+		return biomes[biom_name].path
+	return ""
+
+func _get_biome_params(biom_name: String) -> Dictionary[String, int]:
+	if biomes.has(biom_name):
+		var biom: DualGridResource = biomes[biom_name]
+		return biom.get_params()
+	return {}
+
+func _generate_biomes_resource():
+	var new_biomes: Dictionary[String, DualGridResource] = {}
+	for name in biomes_names:
+		if not biomes.has(name):
+			var biome := DualGridResource.new()
+			biome.biom_name = name
+			biome.init_params(params_names) 
+			new_biomes[name] = biome
+		else: 
+			var biome: DualGridResource = biomes[name]
+			biome.init_params(params_names)
+			new_biomes[name] = biome
+	biomes = new_biomes
+
+func _get_property_list() -> Array[Dictionary]:
+	var props: Array[Dictionary] = []
+	for name in biomes_names:
+		props.append({
+			"name": "biom_%s" % name,
+			"type": TYPE_OBJECT,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "Biom",
+			"usage": PROPERTY_USAGE_DEFAULT
+		})
+	return props
+
+# ========= get =========
+func _get(property: StringName):
+	if property.begins_with("biom_"):
+		var key = property.substr(5)
+		return biomes.get(key, null)
+	return null
+
+# ========= set =========
+func _set(property: StringName, value: Variant) -> bool:
+	if property.begins_with("biom_") and value is DualGridResource:
+		var key = property.substr(5)
+		biomes[key] = value
+		return true
+	return false
