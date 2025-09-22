dc6b7238bf1144a25e40ffc20ab565a2e57db2a0
SimpleSyimyk <117256036+SimpleSyimyk@users.noreply.github.com>
2025-09-22 18:18:58 +0600

    f
    f

--- dualgrid-terrain-generation-main/README.md
+++
@@ -1,2 +0,0 @@
-# dualgrid-terrain-generation
-Library for procedural terrain and world generation using the DualGrid approach. Designed for flexibility, scalability, and easy integration into games or simulations.
--- dualgrid-terrain-generation-main/dual-grid-generator/example/player/player.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-var speed: int = 10000
-var direction: Vector2 = Vector2.ZERO
-
-func _physics_process(delta: float) -> void:
-	direction = Input.get_vector("ui_left", "ui_right", "ui_up", 'ui_down')
-	velocity = direction * speed * delta
-	move_and_slide()
